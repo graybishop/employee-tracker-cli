0d08823bb8195cae91609f3a85a0430cde73d581
Jake Swackhammer <j.100101@gmail.com>
2021-10-21 20:42:31 -0400

    set up seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,22 @@
 -- fake data
+USE employee_tracker_db;
 
-INSERT INTO courses (id, course_title, course_description, active)
-VALUES (001, "Algebra I", "Linear equations, inequalities, functions, and graphs", true),
-       (002, "Pre-Calculus", "Polynomials, Complex Numbers, Vectors", true),
-       (003, "Calculus I", "Limits, Differentiation, Derivatives", true),
-       (004, "Euclidean Geometry", "Intuitively Appealing Axioms Galore", false);
+INSERT INTO departments (dep_name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("IT"),
+       ("Social Media");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Manager", 45000, 1),
+       ("Senior Engineer", 46000, 2),
+       ("Sys Admin", 47000, 3),
+       ("Lead Marketer", 48000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Smith", 1, null),
+       ("Jenny", "Nicholson", 2, null),
+       ("Jarvis", "Johnson", 3, null),
+       ("Drew", "Gooden", 4, null);
+
        
--- index.js
+++ index.js
@@ -11,32 +11,3 @@
 // * Application allows users to view employees by department (2 points).
 // * Application allows users to delete departments, roles, and employees (2 points for each).
 // * Application allows users to view the total utilized budget of a department&mdash;in other words, the combined salaries of all employees in that department (8 points).
-
-//database set-up
-// * `department`
-
-//     * `id`: `INT PRIMARY KEY`
-
-//     * `name`: `VARCHAR(30)` to hold department name
-
-// * `role`
-
-//     * `id`: `INT PRIMARY KEY`
-
-//     * `title`: `VARCHAR(30)` to hold role title
-
-//     * `salary`: `DECIMAL` to hold role salary
-
-//     * `department_id`: `INT` to hold reference to department role belongs to
-
-// * `employee`
-
-//     * `id`: `INT PRIMARY KEY`
-
-//     * `first_name`: `VARCHAR(30)` to hold employee first name
-
-//     * `last_name`: `VARCHAR(30)` to hold employee last name
-
-//     * `role_id`: `INT` to hold reference to employee role
-
-//     * `manager_id`: `INT` to hold reference to another employee that is the manager of the current employee (`null` if the employee has no manager)
