4e7fe64a25412562b8ba002584ea78e792917344
Jake Swackhammer <j.100101@gmail.com>
2021-10-21 21:17:35 -0400

    set up initial connection, set up console.table

--- index.js
+++ index.js
@@ -11,3 +11,26 @@
 // * Application allows users to view employees by department (2 points).
 // * Application allows users to delete departments, roles, and employees (2 points for each).
 // * Application allows users to view the total utilized budget of a department&mdash;in other words, the combined salaries of all employees in that department (8 points).
+import mysql from 'mysql2/promise'
+// eslint-disable-next-line no-unused-vars
+import cTable from 'console.table'
+
+const DB = 'employee_tracker_db'
+
+const main = async () => {
+    const connection = await mysql.createConnection(
+        {
+            host:'localhost', 
+            user: 'root',
+            password: 'blueyellowdonkeysevenkillerbee', 
+            database: DB,
+
+    });
+    // query database
+    const [rows] = await connection.execute('SELECT * FROM roles');
+
+    console.table(rows)
+    connection.end()
+}
+
+main()
