964b91c11f048437a2e0adcae3ddf059398bc310
Jake Swackhammer <j.100101@gmail.com>
2021-10-22 22:25:11 -0400

    created funcs for adding rows to each table

--- src/mysql-helpers.js
+++ src/mysql-helpers.js
@@ -18,7 +18,8 @@ const startConnection = async () => {
 const pullDepartmentTable = async (connection) => {
     const [rows] = await connection.execute(`
     SELECT 
-        dep_name as "Department Name" 
+        dep_name as "Department Name", 
+        id as "Dep ID"
     FROM departments;`);
     return rows;
 };
@@ -51,6 +52,53 @@ const pullEmpTable = async (connection) => {
     return rows;
 };
 
+const addDepartment = async (connection, newDepartment) => {
+    const response = await connection.execute(`
+    INSERT INTO 
+        departments (dep_name)
+    VALUES 
+        (?)
+    `, [newDepartment])
+    return response
+}
+
+/**
+ * Adds role to roles table given role definition object
+ * @param {*} connection MySQL connection
+ * @param {*} title role title 
+ * @param {*} salary role salary
+ * @param {*} dep_id role department
+ * @returns ResultSetHeader
+ */
+const addRole = async (connection, {title, salary, dep_id}) => {
+    const response = await connection.execute(`
+    INSERT INTO 
+        roles (title, salary, department_id)
+    VALUES 
+        (? , ? , ?)
+    `, [title, salary, dep_id])
+    return response
+}
+
+/**
+ * Adds employee to employee table given role definition object
+ * @param {*} connection MySQL connection
+ * @param {*} fName first name 
+ * @param {*} lName last name
+ * @param {*} role_id role id
+ * @param {*} manager_id manager id
+ * @returns ResultSetHeader
+ */
+const addEmployee = async (connection, {fName, lName, role_id, manager_id}) => {
+    const response = await connection.execute(`
+    INSERT INTO 
+        employees (first_name, last_name, role_id, manager_id)
+    VALUES 
+        ( ?, ?, ?, ? )
+    `, [fName, lName, role_id, manager_id])
+    return response
+}
+
 const endConnection = (connection) => {
     connection.end();
 };
@@ -64,6 +112,16 @@ const main = async () => {
     console.table(roleTable);
     let empTable = await pullEmpTable(connection);
     console.table(empTable);
+    // await addDepartment(connection, 'Head Office')
+    // let depTableAfterInsert = await pullDepartmentTable(connection);
+    // console.table(depTableAfterInsert);
+    // await addRole(connection, {title:'IT Guy', salary:30000, dep_id: 3})
+    // let rolesTableAfterInsert = await pullRoleTable(connection);
+    // console.table(rolesTableAfterInsert);
+    await addEmployee(connection, {fName:'Simon', lName:'Lane', role_id: 5, manager_id: 3})
+    let empTableAfterInsert = await pullEmpTable(connection);
+    console.table(empTableAfterInsert);
+
     endConnection(connection);
 };
 
